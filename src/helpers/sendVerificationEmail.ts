27c8980cf8e24e6de7f4edfd55151eb4af310534
Saumya Nayak <saumyo224@gmail.com>
2025-06-17 01:15:34 +0530

    resend email and react email configured and setup done

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,31 @@
+import { resend } from "@/lib/resend";
+import { ApiResponse } from "@/types/ApiResponse";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string,
+): Promise<ApiResponse> {
+    
+    try {
+        resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: 'email',
+        subject: 'Feedback | Verification email',
+        react: VerificationEmail({username, otp: verifyCode})
+    });
+
+    return {
+        success: true,
+        message: "Verification email sent successfully"
+    }
+
+    } catch (error) {
+        console.log(`Verification sending mail error: ${error}`)
+        return {
+            success: false,
+            message: "Failed to send verification email"
+        }
+    }
+}
