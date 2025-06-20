d2ae16cac3ee93c4c113fca12b05a21c286bcba9
Saumya Nayak <saumyo224@gmail.com>
2025-06-20 19:05:15 +0530

    feat: implement POST endpoint for sending user messages

---
+++ src/app/api/send-messages/route.ts
@@ -0,0 +1,54 @@
+import DbConnect from "@/lib/DbConnect";
+import UserModel from "@/models/User";
+import { NextRequest,NextResponse } from "next/server";
+import { messageSchema } from "@/schemas/messageSchema";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { Message } from "@/models/User";
+
+export async function POST(request: NextRequest) {
+    await DbConnect();
+
+    try {
+        const {username, message} = await request.json()
+
+        //zod validation
+        const validUsername = usernameValidation.safeParse(username);
+        if(!validUsername.success){
+            return NextResponse.json({success: false, error: validUsername.error}, {status: 400})
+        }
+
+        const validMessage = messageSchema.safeParse(message);
+        if(!validMessage.success){
+            return NextResponse.json({success: false, error: validMessage.error}, {status: 400})
+        }
+
+        const user = await UserModel.findOne({username});
+
+        if(!user){
+            return NextResponse.json({success: false, error: "User not found. Please check username"}, {status: 404})
+        }
+
+        if(!user.isAcceptingMessages){
+            return NextResponse.json({success: false, error: "User is no longer accepting feedbacks"}, {status: 403})
+        }
+
+        const feedBackMessage = {
+            content: message,
+            createdAt: new Date()
+        } 
+
+        user.messages.push(feedBackMessage as Message );
+        await user.save();
+
+        return NextResponse.json({success: true, message: "Message sent successfully"}, {status: 201})
+
+
+    } catch (error) {
+         console.log("Error sending messages: ", error)
+        if(error instanceof Error){
+            return NextResponse.json({success: false, error: error.message}, {status: 500})
+        }else {
+            return NextResponse.json({success: false, error: "Internal server error"}, {status: 500})
+        }
+    }
+}
