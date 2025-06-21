5d9a6adf5b012a2769e92897f26a7b89496bb202
Saumya Nayak <saumyo224@gmail.com>
2025-06-21 16:47:16 +0530

    feat: add Toaster component for notifications and integrate it into the layout

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,36 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import axios, { isAxiosError } from "axios";
+import { handleFrontendErrors } from "@/helpers/handleFrontendErrors";
 
 function SignUpPage() {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    async function isUsernameUnique() {
+      try {
+        const response = await axios.get("/api/check-unique-username");
+      } catch (error: unknown) {
+        handleFrontendErrors(error);
+      }
+    }
+  }, []);
   return <div>SignUpPage</div>;
 }
 
