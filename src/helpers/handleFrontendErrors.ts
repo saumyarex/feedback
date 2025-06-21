4e61c3bebf4ced36eb1e8d5806cf1234e225f633
Saumya Nayak <saumyo224@gmail.com>
2025-06-21 20:10:58 +0530

    refactor: update error handling to use 'message' for toast notifications

--- src/helpers/handleFrontendErrors.ts
+++ src/helpers/handleFrontendErrors.ts
@@ -4,19 +4,19 @@ import { toast } from "sonner"
 export function handleFrontendErrors(error: unknown, useToast?:boolean){
     console.error(error)
     if (axios.isAxiosError(error)) {
-        console.error(error.response?.data.error);
+        console.error(error.response?.data.message);
         if(useToast){
-          toast(error.response?.data.error)
+          toast.error(error.response?.data.message)
         }
       } else if (error instanceof Error) {
         console.error(error.message);
         if(useToast){
-          toast(error.message);
+          toast.error(error.message);
         }
       } else {
         console.error("An unexpected error occurred")
         if(useToast){
-          toast("An unexpected error occurred");
+          toast.error("An unexpected error occurred");
         }
       }
 }
