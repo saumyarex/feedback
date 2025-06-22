ba53ff727ac6e55535642da7929a502b957e835b
Saumya Nayak <saumyo224@gmail.com>
2025-06-22 18:00:16 +0530

    refactor: remove console error logs from handleFrontendErrors function

--- src/helpers/handleFrontendErrors.ts
+++ src/helpers/handleFrontendErrors.ts
@@ -2,19 +2,16 @@ import axios from "axios";
 import { toast } from "sonner"
 
 export function handleFrontendErrors(error: unknown, useToast?:boolean){
-    console.error(error)
+    //console.log(error)
     if (axios.isAxiosError(error)) {
-        console.error(error.response?.data.message);
         if(useToast){
           toast.error(error.response?.data.message)
         }
       } else if (error instanceof Error) {
-        console.error(error.message);
         if(useToast){
           toast.error(error.message);
         }
       } else {
-        console.error("An unexpected error occurred")
         if(useToast){
           toast.error("An unexpected error occurred");
         }
