eca63e8168b7ef79cafec047b06c4da1df3124f4
Saumya Nayak <saumyo224@gmail.com>
2025-06-23 23:48:53 +0530

    refactor: simplify messageSchema to use a string type directly

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,6 +1,4 @@
 import {z} from "zod"
 
 
-export const messageSchema = z.object({
-    content: z.string().min(10, {message: "Message must be atleast 10 characters"}).max(300, {message: "Message must be less than 300 charaters"})
-})
+export const messageSchema = z.string().min(10, {message: "Message must be atleast 10 characters"}).max(300, {message: "Message must be less than 300 charaters"})
