f22bc1dec1cc8b292e41bcc383e82b2df3d0235c
Saumya Nayak <saumyo224@gmail.com>
2025-06-25 00:25:16 +0530

    feat: add NavBar component with authentication handling and navigation links

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import { signOut, useSession } from "next-auth/react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+function NavBar() {
+  const { data: session } = useSession();
+  console.log(session);
+  return (
+    <nav>
+      <div className="flex flex-col sm:flex-row gap-5 p-5 bg-gray-800 justify-between items-center ">
+        <div className="text-xl font-bold text-neutral-100">
+          <Link href={"/"}>TRUE FEEDBACK</Link>
+        </div>
+        {session ? (
+          <>
+            <span className="text-white text-center font-bold sm:-ml-20">
+              Welcome {session.user.username}
+            </span>
+            <Button
+              className="bg-neutral-200 text-black font-semibold hover:bg-neutral-300 hover:cursor-pointer"
+              onClick={() => signOut()}
+            >
+              Logout
+            </Button>
+          </>
+        ) : (
+          <div className="space-x-5">
+            <Link href={"/sign-in"}>
+              <Button className="bg-neutral-200 text-black font-semibold hover:bg-neutral-300 hover:cursor-pointer">
+                Sign In
+              </Button>
+            </Link>
+
+            <Link href={"/sign-up"}>
+              <Button className="bg-neutral-300 text-black font-semibold hover:bg-neutral-300 hover:cursor-pointer">
+                Sign Up
+              </Button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
