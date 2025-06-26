ffaf5263e24c33c13cea49480843b9a79b202689
Saumya Nayak <saumyo224@gmail.com>
2025-06-26 20:23:03 +0530

    feat: add messageFormSchema to validate message content structure

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -2,3 +2,9 @@ import {z} from "zod"
 
 
 export const messageSchema = z.string().min(10, {message: "Message must be atleast 10 characters"}).max(300, {message: "Message must be less than 300 charaters"})
+
+
+export const messageFormSchema = z.object({
+    content: messageSchema
+}
+) 
