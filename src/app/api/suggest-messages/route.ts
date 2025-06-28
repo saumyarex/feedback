8f8532534da3e62afd03aff8c63f808ded275cc0
Saumya Nayak <saumyo224@gmail.com>
2025-06-28 17:13:12 +0530

    fix: remove unnecessary console log and adjust response method in POST endpoint

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -12,7 +12,6 @@ export async function POST() {
         const model = google('gemini-1.5-flash');
         const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-        console.log("api hit")
 
         const result =  streamText({
             model,
@@ -24,8 +23,8 @@ export async function POST() {
         });
 
 
-        return result.toTextStreamResponse();
-        //return result.toDataStreamResponse();
+        //return result.toTextStreamResponse();
+        return result.toDataStreamResponse();
 
     } catch (error) {
         console.log(error)
